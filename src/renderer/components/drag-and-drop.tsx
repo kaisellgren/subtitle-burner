d76bc96f2a95f5614d28307451870c2108931f4d
Kai Sellgren <kaisellgren@gmail.com>
2025-02-16 21:22:21 +0200

    Add DragAndDrop component

---
+++ src/renderer/components/drag-and-drop.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+interface DragOverlayProps {
+  $isDragging: boolean
+}
+
+const DragOverlay = styled.div<DragOverlayProps>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: ${(p) => p.theme.palette.primary.main};
+  opacity: 0.5;
+  z-index: 1000000;
+  border: 0.4rem dashed white;
+`
+
+interface Props {
+  onDropFiles: (files: File[]) => void
+}
+
+export function DragAndDrop({ onDropFiles }: Props) {
+  const [isDragging, setIsDragging] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('dragover', (event) => {
+      event.preventDefault()
+      event.stopPropagation()
+      event.dataTransfer.dropEffect = 'copy'
+
+      if (!isDragging) {
+        setIsDragging(true)
+      }
+    })
+
+    window.addEventListener('dragleave', (event) => {
+      if (event.relatedTarget == null) {
+        event.preventDefault()
+        event.stopPropagation()
+        setIsDragging(false)
+      }
+    })
+
+    window.addEventListener('drop', async (event) => {
+      event.preventDefault()
+      event.stopPropagation()
+      setIsDragging(false)
+
+      onDropFiles(Array.from(event.dataTransfer.files))
+    })
+  }, [])
+
+  return isDragging ? <DragOverlay $isDragging={isDragging} /> : <></>
+}
