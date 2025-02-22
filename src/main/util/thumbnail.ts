6854d10524161d910303336b305c965071eaaa5f
Kai Sellgren <kaisellgren@gmail.com>
2025-02-22 17:45:02 +0200

    Add thumbnail utility

---
+++ src/main/util/thumbnail.ts
@@ -0,0 +1,16 @@
+import Buffer from 'node:buffer'
+import { $, tempfile } from 'zx'
+import { promises as fs } from 'fs'
+
+export async function createThumbnail(fullPath: string, videoDuration: number): Promise<Buffer> {
+  const thumbnailFile = `${tempfile()}.avif`
+
+  await $`ffmpeg -ss ${Math.round(videoDuration / 2)} -noaccurate_seek -i ${fullPath} -read_ahead_limit 1 -skip_frame nokey -an -vf "thumbnail,scale=320:-1" -frames:v 1 -c:v libaom-av1 -crf 40 -still-picture 1 ${thumbnailFile}`.quiet()
+
+  return fs.readFile(thumbnailFile)
+}
+
+export async function createDataUriThumbnail(fullPath: string, videoDuration: number): Promise<string> {
+  const data = await createThumbnail(fullPath, videoDuration)
+  return `data:image/avif;base64,${data.toString('base64')}`
+}
